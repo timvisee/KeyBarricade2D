31e256732d865773906ec0a1a5df529e55d4354e
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-03-22 13:25:14 +0100

    Create base player class

---
+++ src/me/KeyBarricade2D/Player/Player.java
@@ -0,0 +1,20 @@
+package me.KeyBarricade2D.Player;
+
+public class Player {
+
+    public void moveUp() {
+        //
+    }
+
+    public void moveDown() {
+        //
+    }
+
+    public void moveLeft() {
+        //
+    }
+
+    public void moveRight() {
+        //
+    }
+}
