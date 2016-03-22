7cc2fdf7d3dfedb4138cdaf1c43b19e3ceeae035
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-03-22 13:18:20 +0100

    Create tile switch, update level matrix

--- src/me/KeyBarricade2D/Level/Level1.java
+++ src/me/KeyBarricade2D/Level/Level1.java
@@ -7,14 +7,14 @@ public class Level1 extends BaseLevel {
     private int [][] speelveld =
             {
                     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 5, 0, 0, 0, 0, 0, 0, 0, 1},
                     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                    {1, 0, 0, 0, 0, 2, 2, 2, 2, 1},
+                    {1, 0, 0, 0, 0, 2, 0, 3, 4, 1},
+                    {1, 0, 0, 0, 0, 2, 0, 2, 2, 1},
+                    {1, 0, 0, 0, 0, 2, 0, 0, 0, 1},
+                    {1, 0, 0, 0, 0, 2, 3, 2, 3, 1},
+                    {1, 4, 0, 0, 0, 0, 0, 2, 6, 1},
                     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
             };
 
@@ -23,14 +23,43 @@ public class Level1 extends BaseLevel {
 
         for (int i = 0; i < speelveld.length; i++) {
             for (int j = 0; j < speelveld.length; j++) {
-                if(speelveld[i][j] == 0){
-                    g.setColor(Color.RED);
-                }
-                if(speelveld[i][j] == 1){
-                    g.setColor(Color.green);
+
+                /**
+                 * Color legend
+                 * ------------
+                 * 0: Default tile
+                 * 1: Border tile
+                 * 2: Wall tile
+                 * 3: Barricade tile
+                 * 4: Key tile
+                 * 5: Player tile
+                 * 6: Exit tile
+                 * ------------
+                 */
+                switch (speelveld[i][j]) {
+                    case 1:
+                        g.setColor(Color.GREEN);
+                        break;
+                    case 2:
+                        g.setColor(Color.BLUE);
+                        break;
+                    case 3:
+                        g.setColor(Color.YELLOW);
+                        break;
+                    case 4:
+                        g.setColor(Color.BLACK);
+                        break;
+                    case 5:
+                        g.setColor(Color.MAGENTA);
+                        break;
+                    case 6:
+                        g.setColor(Color.CYAN);
+                        break;
+                    default:
+                        g.setColor(Color.RED);
+                        break;
                 }
                 g.fillRect(40 + j * 40, 40 + i * 40, 40, 40);
-
             }
         }
     }
