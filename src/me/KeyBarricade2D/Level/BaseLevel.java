96adbbe574c5e950e678da4f0e637a11a5628a11
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-03-22 11:06:57 +0100

    Create Baselevel and level 1

---
+++ src/me/KeyBarricade2D/Level/BaseLevel.java
@@ -0,0 +1,8 @@
+package me.KeyBarricade2D.Level;
+
+public abstract class BaseLevel {
+
+    public abstract void paint();
+    public abstract void start();
+    public abstract void update();
+}
---
+++ src/me/KeyBarricade2D/Level/Level1.java
@@ -0,0 +1,19 @@
+package me.KeyBarricade2D.Level;
+
+public class Level1 extends BaseLevel {
+
+    @Override
+    public void paint() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
