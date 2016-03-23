e7b17b73503501634f9ed7cd0029bfede084cf22
SimonHaasnoot <simonhaasnoot@hotmail.com>
2016-03-23 19:10:08 +0100

    implemented MapLoader + testmap

--- src/me/KeyBarricade2D/Level/Level2.java
+++ src/me/KeyBarricade2D/Level/Level2.java
@@ -5,46 +5,31 @@ import me.KeyBarricade2D.Player.Player;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
+import java.io.*;
 
 
 public class Level2 extends BaseLevel {
 
-    private int[][] speelveld =
-            {
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-            };
+    private int[][] map;
 
     private BufferedImage grass;
     private BufferedImage stone;
 
     private Player player;
 
+    private int mapWidth;
+    private int mapHeight;
+
     @Override
     public void paint(Graphics2D g) {
 
-        for (int i = 0; i < speelveld.length; i++) {
-            for (int j = 0; j < speelveld.length; j++) {
+        for (int i = 0; i < mapHeight; i++) {
+            for (int j = 0; j <  mapWidth; j++) {
 
-                switch (speelveld[i][j]) {
+                switch (map[i][j]) {
 
                     case 1:
                         g.drawImage(stone, 10 + j * 40, 10 + i * 40, 40, 40, null);
-                        break;
-
-                    case 2:
-                        g.setColor(Color.BLUE);
                         break;
 
                     default:
@@ -60,16 +45,35 @@ public class Level2 extends BaseLevel {
     @Override
     public void start() {
 
+        loadMap();
         player = new Player(210, 210, 40, 40);
+
         stone = loadImage("stone.png");
-        grass = loadImage("grass.png");
+        grass = loadImage("tegel.jpg");
     }
 
 
-    @Override
-    public void update() {
+    public void loadMap(){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("testmap.txt"));
 
-        player.registerMovement();
+            mapWidth = Integer.parseInt(br.readLine());
+            mapHeight = Integer.parseInt(br.readLine());
+
+            map = new int[mapHeight][mapWidth];
+
+            for (int i = 0; i < mapHeight; i++) {
+                String line = br.readLine();
+                String [] numbers = line.split("\\s+");
+
+                for (int j = 0; j < mapWidth; j++) {
+                    map[i][j] = Integer.parseInt(numbers[j]);
+
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public BufferedImage loadImage(String filename) {
@@ -79,5 +83,11 @@ public class Level2 extends BaseLevel {
         } catch(IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public void update() {
+
+        player.registerMovement();
     }
 }
