3c7b07019b33a1d15e677a6c26f5a19ecb985124
SimonHaasnoot <simonhaasnoot@hotmail.com>
2016-03-28 16:58:12 +0200

    Add player X and Y getters/setters

--- src/me/KeyBarricade2D/Player/Player.java
+++ src/me/KeyBarricade2D/Player/Player.java
@@ -43,6 +43,21 @@ public class Player {
         }
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 
     public void paint(Graphics2D g){
         g.fillRect(x, y, width, height);
