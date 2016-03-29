34a6ff8158b71d46cd60e748066ee3dc008a27a9
SimonHaasnoot <simonhaasnoot@hotmail.com>
2016-03-29 16:44:21 +0200

    Change tileType goal

--- src/me/KeyBarricade2D/Level/Tiles/Goal.java
+++ src/me/KeyBarricade2D/Level/Tiles/Goal.java
@@ -5,11 +5,12 @@ import java.awt.image.BufferedImage;
 public class Goal extends Tile {
 
     private BufferedImage image;
-    public final int TILETYPE = 4;
 
     public Goal(BufferedImage image) {
         super(image);
         this.image = image;
+
+        tileType = 4;
     }
 
     public void finish() {
