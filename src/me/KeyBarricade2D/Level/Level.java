1e4cd33dc52e2376986098ea9998e376c6d0b068
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-03-29 12:01:34 +0200

    Improve bracket spacing

--- src/me/KeyBarricade2D/Level/Level.java
+++ src/me/KeyBarricade2D/Level/Level.java
@@ -44,29 +44,29 @@ public class Level {
 
     }
 
-    public void checkCollision(){
+    public void checkCollision() {
 
         for (int i = 0; i < mapHeight; i++) {
             for (int j = 0; j < mapWidth; j++) {
 
-                if((player.getX() == (j * tileSize + 10) && player.getY() == (i * tileSize + 10) && map[i][j].tileType == 1)){
+                if((player.getX() == (j * tileSize + 10) && player.getY() == (i * tileSize + 10) && map[i][j].tileType == 1)) {
 
-                    if(player.isMovingLeft){
+                    if(player.isMovingLeft) {
                         player.setX(player.getX() + 40);
                         player.isMovingLeft = false;
                     }
 
-                    if(player.isMovingRight){
+                    if(player.isMovingRight) {
                         player.setX(player.getX() -40);
                         player.isMovingRight = false;
                     }
 
-                    if(player.isMovingUp){
+                    if(player.isMovingUp) {
                         player.setY(player.getY() + 40);
                         player.isMovingUp = false;
                     }
 
-                    if(player.isMovingDown){
+                    if(player.isMovingDown) {
                         player.setY(player.getY() - 40);
                         player.isMovingDown = false;
                     }
@@ -76,7 +76,7 @@ public class Level {
         }
     }
 
-    public void loadMap(){
+    public void loadMap() {
 
         try {
             BufferedReader br = new BufferedReader(new FileReader("testmap.txt"));
@@ -122,7 +122,6 @@ public class Level {
         player.registerMovement();
 
         checkCollision();
-
     }
 
     public void paint(Graphics2D g) {
