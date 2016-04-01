92de55e212ca08d12af300be522b5cbf02e2b8e4
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-04-01 15:30:57 +0200

    Create keyboard class, shorten keypress actions and shorten return statement

---
+++ src/me/KeyBarricade2D/input/key/Keyboard.java
@@ -0,0 +1,16 @@
+package me.KeyBarricade2D.input.key;
+
+import java.awt.event.KeyEvent;
+
+public class Keyboard {
+
+    public static final int W = KeyEvent.VK_W;
+    public static final int A = KeyEvent.VK_A;
+    public static final int S = KeyEvent.VK_S;
+    public static final int D = KeyEvent.VK_D;
+
+    public static final int UP = KeyEvent.VK_UP;
+    public static final int LEFT = KeyEvent.VK_LEFT;
+    public static final int DOWN = KeyEvent.VK_DOWN;
+    public static final int RIGHT = KeyEvent.VK_RIGHT;
+}
